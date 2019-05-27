17acbbc58a3583d736544b89cbeb6073996b20c5
juliozarate5 <juliozarate5@yahoo.com>
2019-05-27 00:35:10 -0500

    Update gestures.js

--- www/js/gestures.js
+++ www/js/gestures.js
@@ -1,13 +1,17 @@
 var emisora = document.getElementById('emisora');
 
 var app = {
+    // primera función que se ejecutar al cargar la pagina
     inicio: function () {
         this.iniciaBotones();
         this.iniciaFastClick();
     },
+    //inicia librería FastClick
     iniciaFastClick: function () {
         FastClick.attach(document.body);
     },
+    
+    //inicializa cada botón en el DOM
     iniciaBotones: function () {
         var botonPlay = document.querySelector('#play');
         var botonPause = document.querySelector('#pausa');
@@ -19,23 +23,29 @@ var app = {
         botonVolMas.addEventListener('click', app.subeVol, false);
         botonVolMenos.addEventListener('click', app.bajaVol, false);
     },
+    
     iniciaHammer: function () {
 
     },
+    // función del botón play del reproductor
     play: function () {
             document.getElementById('emisora').play();
     },
+    // función del botón pausa del reproductor
     pausa: function () {     
             document.getElementById('emisora').pause(); 
     },
+    // función del botón subir volume del reproductor
     subeVol: function () {
         document.getElementById('emisora').volume += 0.1;
     },
+    // función del botón bajar volume del reproductor
     bajaVol: function () {
         document.getElementById('emisora').volume -= 0.1;
     }
 };
 
+// inicializar al cargar la pagina
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function () {
         app.inicio();
